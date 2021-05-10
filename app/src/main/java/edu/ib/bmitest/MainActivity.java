10aeda2496adc9f2e06f91019d26961467118693
MiroslawLatka <MiroslawLatka@pwr.edu.pl>
2021-05-10 08:15:47 +0200

    Initial commit

---
+++ app/src/main/java/edu/ib/bmitest/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.ib.bmitest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
